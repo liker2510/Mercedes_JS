7bf0dafb8241f970241fc4f04259ab56b9bb96a6
liker2510 <81087122+liker2510@users.noreply.github.com>
2021-09-22 22:55:50 +0500

    Add files via upload

--- JS/accordeon.js
+++ JS/accordeon.js
@@ -1,19 +1,16 @@
-const lists = document.querySelectorAll('.feature-sub');
-const btns = document.querySelectorAll('.feature__link');
+const lists = document.querySelectorAll('.feature-sub')
+const btns = document.querySelectorAll('.feature__link')
 
-//btns.addEventListener('click', () => {});
-
-btns.forEach((btnItem, index) => {
-    btnItem.addEventListener('click', () => {
-        btns.forEach((btnItem) => {
-            btnItem.classList.remove('feature__link_active')
+btns.forEach((btn,) => {
+    btn.addEventListener('click', () => {
+        btns.forEach((btnItem, idx) => {
+            if (btnItem === btn) {
+                btnItem.classList.toggle('feature__link_active')
+                lists[idx].classList.toggle('hidden')
+            } else {
+                btnItem.classList.remove('feature__link_active')
+                lists[idx].classList.add('hidden')
+            }
         })
-        btnItem.classList.add('feature__link_active')
-
-        lists.forEach((listItem) => {
-            listItem.classList.add('hidden')
-        })
-
-        lists[index].classList.remove('hidden')
     })
 })
